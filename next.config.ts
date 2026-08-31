56a8096a8166844f523422ce452e112651c2e40f
Niklas Weidl <nik.weidl@gmail.com>
2026-08-31 15:56:44 +0200

    chore: disable next.js development indicator

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  devIndicators: false,
   turbopack: {
     root: path.resolve(__dirname),
   },
